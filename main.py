a5df5bdc3c68e478e63e05e011c5f8225244f670
datalytix-777 <user1@Computer.localdomain>
2025-06-08 19:42:55 -0400

    utilized os import and corrected formatting issues

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from pydantic import BaseModel, Field
 from ml.data import apply_label, process_data
 from ml.model import inference, load_model
 
+
 # DO NOT MODIFY
 class Data(BaseModel):
     age: int = Field(..., example=37)
@@ -26,13 +27,17 @@ class Data(BaseModel):
     hours_per_week: int = Field(..., example=40, alias="hours-per-week")
     native_country: str = Field(..., example="United-States", alias="native-country")
 
-path = "model/encoder.pkl"
+
+project_path = "/home/user1/Deploying-a-Scalable-ML-Pipeline-with-FastAPI"
+
+path = os.path.join(project_path, "model", "encoder.pkl")
 encoder = load_model(path)
 
-path = "model/model.pkl" 
+path = os.path.join(project_path, "model", "model.pkl")
 model = load_model(path)
 
 app = FastAPI()
+
 
 @app.get("/")
 async def get_root():
@@ -45,7 +50,7 @@ async def post_inference(data: Data):
     # DO NOT MODIFY: turn the Pydantic model into a dict.
     data_dict = data.dict()
     # DO NOT MODIFY: clean up the dict to turn it into a Pandas DataFrame.
-    # The data has names with hyphens and Python does not allow those as variable names.
+    # data has names with hyphens, Python does not allow those as var names.
     # Here it uses the functionality of FastAPI/Pydantic/etc to deal with this.
     data = {k.replace("_", "-"): [v] for k, v in data_dict.items()}
     data = pd.DataFrame.from_dict(data)
@@ -61,14 +66,10 @@ async def post_inference(data: Data):
         "native-country",
     ]
     data_processed, _, _, _ = process_data(
-        # your code here
-        # use data as data input
-        # use training = False
-        # do not need to pass lb as input
         X=data,
         categorical_features=cat_features,
         training=False,
         encoder=encoder
     )
-    _inference = inference(model, data_processed) # your code here to predict the result using data_processed
+    _inference = inference(model, data_processed)
     return {"result": apply_label(_inference)}
